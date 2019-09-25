ead7eabf36cf7f24332c364072c41db0b430dfd5
Han Kim <hankim@caltech.edu>
2019-09-25 13:28:32 -0700

    Used conditionals to enable parallel movements of motors

--- code/beetle_toucher.ino
+++ code/beetle_toucher.ino
@@ -12,6 +12,7 @@
 #include <SPI.h>
 #include <HighPowerStepperDriver.h>
 #include <Servo.h>
+# include <AccelStepper.h>
 
 const uint8_t DirPin = 2;
 const uint8_t StepPin = 3;
@@ -22,10 +23,11 @@ const uint8_t CSPin = 4;
 // go slower.  If you decrease the delay, the stepper motor will go faster, but
 // there is a limit to how fast it can go before it starts missing steps.
 const uint16_t StepPeriodUs = 2000;
+unsigned int stepper_pos = 6000;
 
 // LIN ACT:
 Servo myservo;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position
+int servo_pos = 0;    // variable to store the servo position
 //
 
 HighPowerStepperDriver sd;
@@ -69,6 +71,37 @@ void setup()
 
 void loop()
 {
+  // Step the stepper motor in the default direction n times
+  setDirection(0); 
+  if (unsigned int x >= 0 and x < stepper_pos)
+  {
+    step();
+    delayMicroseconds(StepPeriodUs);
+  }
+  delay(300);
+  /*
+  // Step the stepper motor in the opposite direction n times
+  setDirection(1); 
+  if (unsigned int x >= 0 and x < stepper_pos)
+  {
+    step();
+    delayMicroseconds(StepPeriodUs);
+  }
+  delay(300);
+  */
+  // Extend the lienar actuator if in bounds, retract if out of bounds:
+  if (servo_pos <= 0 and servo_pos >= 100)
+  {
+    myservo.write(0);
+    delay(15);
+  }
+  else 
+  {
+    myservo.write(101);
+    delay(15);
+  }
+  
+  /*
   // Step in the default direction 6000 times.
   setDirection(0);
   for(unsigned int x = 0; x < 6000; x++)
@@ -92,16 +125,18 @@ void loop()
   delay(300);
 
   // LIN ACT
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (servo_pos = 0; servo_pos <= 180; servo_pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo.write(servo_pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  for (servo_pos = 180; servo_pos >= 0; servo_pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(servo_pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+  */
 }
+
 
 // Sends a pulse on the STEP pin to tell the driver to take one step, and also
 //delays to control the speed of the motor.
