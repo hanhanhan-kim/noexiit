8a67205433822238b795330d81a48d3ad1a049d4
han_parkerlab <hankim@caltech.edu>
2020-01-17 13:44:22 -0800

    Added in reading of servo calibration value

--- python_scripts/home_pt_to_pt_servo.py
+++ python_scripts/home_pt_to_pt_servo.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 from autostep import Autostep
-from calibrate import max_ext
 import time
 import threading
 import numpy as np
@@ -15,8 +14,8 @@ motor_port = '/dev/ttyACM0' # change as necessary
 stepper = Autostep(motor_port)
 stepper.set_step_mode('STEP_FS_128') 
 stepper.set_fullstep_per_rev(200)
-stepper.set_jog_mode_params({'speed':60, 'accel':100, 'decel':1000}) # deg/s and deg/s2
 stepper.set_kval_params({'accel':30, 'decel':30, 'run':30, 'hold':30})
+stepper.set_jog_mode_params({'speed':60, 'accel':100, 'decel':1000}) # deg/s and deg/s2
 stepper.set_move_mode_to_jog()
 stepper.set_gear_ratio(1)
 stepper.enable() 
@@ -31,8 +30,9 @@ def pt_to_pt_and_servo(pos_list, ext_angle, wait_time):
         wait_time: (float) duration of time (s) for which to wait at each position in pos_list
     Returns nothing. 
     '''
-    angle_list_fwd = list(np.linspace(0, ext_angle, ext_angle))
+    angle_list_fwd = list(np.linspace(0, ext_angle, int(ext_angle)))
     angle_list_rev = list(angle_list_fwd[::-1])
+    dt = 0.01
 
     for _, pos in enumerate(pos_list):
         # Move stepper to pos:
@@ -42,22 +42,24 @@ def pt_to_pt_and_servo(pos_list, ext_angle, wait_time):
         for _, angle in enumerate(angle_list_fwd):
             stepper.set_servo_angle(angle)
             while stepper.get_servo_angle() <= ext_angle is True:
-                time.sleep(0.01)
+                time.sleep(dt)
         # Wait at extension:
         time.sleep(wait_time)
         # Retract linear servo:
         for _, angle in enumerate(angle_list_rev):
             stepper.set_servo_angle(angle)
             while stepper.get_servo_angle() >= 0 is True:
-                time.sleep(wait_time)
+                time.sleep(dt)
         # Wait at retraction:
         time.sleep(wait_time)
 
 
 # Arguments for above function:
 pos_list = [0.0, 180.0, 360.0, 2*360, 540.0]
-ext_angle = max_ext 
 wait_time = 5.0
+with open ("calib_servo.noexiit", "r") as f:
+    max_ext = f.read().rstrip('\n')
+ext_angle = float(max_ext)
 
 # Set the home position to 0:
 print('Searching for home...')
