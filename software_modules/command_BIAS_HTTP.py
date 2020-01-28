169aee04f10886dfaf27ef2cb87a1f446b56feeb
han_parkerlab <hankim@caltech.edu>
2020-01-28 11:15:04 -0800

    Added explicit port and retries arguments

--- software_modules/command_BIAS_HTTP.py
+++ software_modules/command_BIAS_HTTP.py
@@ -4,20 +4,21 @@ import sys
 import time
 
 
-def command_BIAS_HTTP(cmd, success_msg, fail_msg):
+def command_BIAS_HTTP(port, cmd, success_msg, fail_msg, retries=10):
 
     """
+    port (str): The port number of the target camera 
     cmd (str): The HTTP get command to use with BIAS
     success_msg (str): The message if port connection and HTTP response both succeed.
     fail_msg (str): The message if port connection and HTTP both fail.
     check_success (bool): Pass as true, to check the "success" key in the BIAS request response.
+    retries (int): The number of retries if connection is 404 or request response has a False value to the success key.
     """
 
     ret = requests.get("http://localhost:" + port + f"/?{cmd}")
 
     ret_dict = ret.json()[0]
 
-    retries = 10
     success = False
     while retries > 0:
         if ret.status_code == 200 and ret_dict.get('success'):
