527c93d9c698e7869d3554a41db0d6d1ca22f5fd
hanhanhan-kim <hankim@caltech.edu>
2020-04-06 15:44:08 -0700

    Add bokeh plotting obj utility

--- scripts/analyses/fourier_transform.py
+++ scripts/analyses/fourier_transform.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 """
-Fourier transform and inverse fourier transform offline data with helper \
-functions.
-Originally written by Kellan P Moorse. 
-
+Perform and visualize Fourier transforms and inverse Fourier transforms on \
+offline data. 
 """
 
 import numpy as np
 import scipy.signal as sps
 import scipy.interpolate as spi
 
+from bokeh.plotting import figure
+
 
 def expceil(x, a=2):
     """
-    Return the smallest power of 2 greater than or equal to a number
+    Return the smallest power of 2 greater than or equal to a number.
+    Written by Kellan P Moorse. 
     """
 
     temp = np.log(x)/np.log(a)
@@ -25,6 +26,7 @@ def fft(ft, t, pad=0, window=None, hilbert=False, post=False):
     """
     Take the fourier transform of the windowed input function. 
     Return amplitude, phase, frequency-spacing. 
+    Written by Kellan P Moorse. 
     """
 
     # Extract sample period
@@ -68,6 +70,7 @@ def ifft(ff, f, pad=0, window=None):
     """
     Take the inverse fourier transform of the windowed input function.
     Return the fourier transform and time domain. 
+    Written by Kellan P Moorse. 
     """
 
     # Extract sample period
@@ -89,3 +92,56 @@ def ifft(ff, f, pad=0, window=None):
     t = np.fft.fftfreq(N, df)
 
     return (ft, t)
+
+
+def bokeh_freq_domain(freq, amp):
+    """
+    Plot the frequency domain on both linear and log scales. 
+    Returns a Bokeh plotting object. Does not output actual plots. 
+
+    Parameters:
+    amp (list or np array): The amplitude of the Fourier transformed data.
+    freq (list or np array): The frequency of the Fourier transformed data. 
+
+    Returns:
+    p:  A bokeh plotting object. 
+    """
+
+    title = "Frequency domain"
+    
+    # Frequency domain:
+    p1 = figure(
+            background_fill_color="#efe8e2",
+            title=title,
+            width=1000,
+            height=500,
+            y_axis_label="power"
+    )
+    p1.line(
+        x=freq,
+        y=amp,
+        color="darkgray"
+    )
+    p2 = figure(
+            background_fill_color="#efe8e2",
+            width=1000,
+            height=500,
+            x_axis_label="frequency (Hz)",
+            y_axis_label="log power",
+            y_axis_type="log"
+    )
+    p2.line(
+        x=freq,
+        y=amp,
+        color="darkgray"
+    )
+
+    return p1, p2
+
+
+# def main():
+
+
+
+# if __name__ == "__main__":
+#     main()
