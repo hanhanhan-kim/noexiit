77a5234ff02cf4efb39ba4afd8cbf4aafdc338cd
Han Kim <hankim@caltech.edu>
2020-06-29 10:12:39 -0700

    change strings to fstrings

--- software/acquisition/init_BIAS.py
+++ software/acquisition/init_BIAS.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+Initialize Will Dickson's BIAS (Basic Image Acquisition Software) for multi-cam,
+externally synchronized video recordings. Loads a .json configuration file. 
+"""
+
 import time
 import json
 import threading
@@ -11,38 +16,40 @@ from start_trigger import start_trigger
 def init_BIAS(cam_ports, config_path, backoff_time=1.0):
 
     """
-    Initializes BIAS with HTTP commands. In BIAS, connects cameras, then loads specified 
-    json configuration file, then prompts user before starting frame capture.
-    If the json specifies external triggering, frame capture will not initiate acquisition.
-    Will instead wait and listen for an external triggering cue. 
+    Initializes BIAS with HTTP commands. In BIAS, connects cameras, then loads 
+    specified json configuration file, then prompts user before starting frame 
+    capture. If the json specifies external triggering, frame capture will not 
+    initiate acquisition. Will instead wait and listen for an external triggering 
+    cue. 
 
     Parameters:
-
+    -----------
     cam_ports (list): A list of strings specifying the server of each camera port. 
+
     config_path (str): The path to the json configuration file for BIAS.
+
     backoff_time (fl): The interval of time between each BIAS HTTP command.
     """
 
-    
     # Connect cameras:
-    for _, port in enumerate(cam_ports):
+    for port in cam_ports:
         command_BIAS(
             port = port,
             cmd = "connect", 
-            success_msg = "Camera on port " + f"{port}" + " connected", 
-            fail_msg = "Port" + f"{port}" + " not connected"
+            success_msg = f"Camera on port {port} connected", 
+            fail_msg = f"Port {port} not connected"
         )
         time.sleep(backoff_time)
 
     # Load json configuration file:
-    for _, port in enumerate(cam_ports):
+    for port in cam_ports:
 
         # First check if the target json is already loaded:
         current_json = command_BIAS(
             port = port,
             cmd = "get-configuration",
-            success_msg = "Got config json on port " + f"{port}",
-            fail_msg = "Could not get config json on port " + f"{port}"
+            success_msg = f"Got config json on port {port}",
+            fail_msg = f"Could not get config json on port {port}"
         ).get("value")
         
         time.sleep(backoff_time)
@@ -57,9 +64,9 @@ def init_BIAS(cam_ports, config_path, backoff_time=1.0):
             print(f"Current json on port {port} is not the target json. Loading target json ...")
             command_BIAS(
                 port = port,
-                cmd = "load-configuration" + '=' + config_path,
-                success_msg = "Loaded configuration json on port " + f"{port}",
-                fail_msg = "Could not load configuration json on port " + f"{port}"
+                cmd = f"load-configuration={config_path}",
+                success_msg = f"Loaded configuration json on port {port}",
+                fail_msg = f"Could not load configuration json on port {port}"
             )
             time.sleep(backoff_time)
 
@@ -82,14 +89,14 @@ def init_BIAS(cam_ports, config_path, backoff_time=1.0):
 
     # Capture frames:
     if skip is False:
-        for _, port in enumerate(cam_ports):
+        for port in cam_ports:
 
             # First check if camera is already capturing:
             status_dict = command_BIAS(
                 port = port,
                 cmd = "get-status",
-                success_msg = "Got status on port " + f"{port}",
-                fail_msg = "Could not get status on port " + f"{port}"
+                success_msg = f"Got status on port {port}",
+                fail_msg = f"Could not get status on port {port}"
             )
             time.sleep(backoff_time)
 
@@ -103,8 +110,8 @@ def init_BIAS(cam_ports, config_path, backoff_time=1.0):
                 command_BIAS(
                     port = port,
                     cmd = "start-capture",
-                    success_msg = "Started acquisition on port " + f"{port}",
-                    fail_msg = "Could not start acquisition on port " + f"{port}"
+                    success_msg = f"Started acquisition on port {port}",
+                    fail_msg = f"Could not start acquisition on port {port}"
                 )
 
         # Config json specifies an external trigger. 
