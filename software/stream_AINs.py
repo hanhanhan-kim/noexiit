10201ea4041d8abb5be9c33f7172aca49a406e3b
Han Kim <hankim@caltech.edu>
2021-01-07 19:08:39 -0800

    restyle

--- software/stream_AINs.py
+++ software/stream_AINs.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+"""
+Utility functions for using LabJack U3 DAQ's streaming abilities. 
+
+Modified from Tom O'Connell's LabJack code:
+https://github.com/ejhonglab/labjack/blob/main/src/labjack/labjack.py
+"""
 
 from __future__ import print_function, division
 
@@ -31,29 +38,32 @@ resolution_index2max_scan_freq = {
 # "Counts".
 
 def get_channel_name(device, channel_index):
+
     """
-    Args:
+    Parameters:
+    -----------
     device (subclass of `LabJackPython.Device`, like `u3.U3`): used to determine
         name and hardwareVersion of device, via `device.deviceName` and
         `device.hardwareVersion`.
 
     channel_index (int): index of (input) channel to get a name for.
 
-    Returns `str` name of (input) channel, as printed on device case.
-
-    Raises `ValueError` for several types of invalid input.
+    Returns:
+    --------
+    `str` name of (input) channel, as printed on device case.
     """
+
     if not issubclass(type(device), Device):
-        raise ValueError('device must be a subclass of LabJackPython.Device')
+        raise ValueError("device must be a subclass of LabJackPython.Device")
 
     if type(channel_index) is not int:
-        raise ValueError('channel_index must be an int')
+        raise ValueError("channel_index must be an int")
 
     if channel_index < 0:
-        raise ValueError('channel_index must be positive')
+        raise ValueError("channel_index must be positive")
 
     device_name = device.deviceName
-    if device_name != 'U3-HV':
+    if device_name != "U3-HV":
         raise NotImplementedError('only U3-HV currently supported')
 
     ############################################################################
@@ -68,32 +78,35 @@ def get_channel_name(device, channel_index):
         )
 
     if channel_index <= 3:
-        return 'AIN{}'.format(channel_index)
+        return f"AIN{channel_index}"
     elif channel_index <= 7:
-        return 'FIO{}'.format(channel_index)
+        return f"FIO{channel_index}"
     else:
-        raise ValueError(('channel index of {} is not a valid input on device '
-            '{}').format(channel_index, device_name)
-        )
+        raise ValueError(f"channel index of {channel index} is not a valid input on device {device_name}.")
     ############################################################################
 
     # TODO also support at least the other U3 variants ('U3-LV' at least, maybe
     # also whatever 'U3B' is (see setting of deviceName in u3.py)
 
 
-# TODO implement callbacks that can be passed into this fn, then pass stuff to
-# publish data from ROS wrapper of this script? or should i just relax the
-# separation of ROS stuff from this file?
-# TODO TODO emulate some kind of triggered acquisition functionality by starting
-# a counter on the trigger pin, and then just only start appending to CSV once
-# the count is >0 (and also start counting towards duration_s from that point)
-# (would need a pin on my stimulus control arduino to go high at the start of
-# the stimulus program though...)
 def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     resolution_index=0, input_channel_names=None, time_col=True,
     overwrite=False, verbose=False):
+
+    # TODO implement callbacks that can be passed into this fn, then pass stuff to
+    # publish data from ROS wrapper of this script? or should i just relax the
+    # separation of ROS stuff from this file?
+    # TODO TODO emulate some kind of triggered acquisition functionality by starting
+    # a counter on the trigger pin, and then just only start appending to CSV once
+    # the count is >0 (and also start counting towards duration_s from that point)
+    # (would need a pin on my stimulus control arduino to go high at the start of
+    # the stimulus program though...)
+
     """
-    Args:
+    Stream AIN data. Can stream directly to a CSV.
+
+    Parameters:
+    ------------
     csv_path (str): path of CSV to stream data to.
 
     duration_s (None or float): If `float`, will stop streaming to CSV after
@@ -125,16 +138,19 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
 
     verbose (bool): If `True`, will print more output. Defaults to `False`.
     """
+
     # TODO also allow use of FIO<4-7> inputs? configure as inputs, etc.
     # `int` in [0, 3]. Should correspond to AIN[0-3] labels on U3-HV.
+
     if input_channels is None:
         input_channels = [0]
+
     # TODO print string label of each of above channels on particular hardware.
     # maybe assert hardware is HV version, if accessible via API.
 
     if duration_s is not None:
         if duration_s <= 0:
-            raise ValueError('duration_s must be positive or None')
+            raise ValueError("duration_s must be positive or None")
 
     max_scan_freq = resolution_index2max_scan_freq[resolution_index]
 
@@ -143,6 +159,7 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     # <d>.streamConfig (SamplesPerPacket, InternalStreamClockFrequency,
     # DivideClockBy256, ScanInterval)
     # ScanFrequency "sample rate (Hz) = ScanFrequency * NumChannels"
+
     scan_frequency = max_scan_freq / len(input_channels)
 
     d = u3.U3()
@@ -152,8 +169,7 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     d.configU3()
 
     # TODO need to actually do some physical calibration procedure first? how to
-    # test for that?
-    # For applying the proper calibration to readings.
+    # test for that? For applying the proper calibration to readings.
     d.getCalibrationData()
 
     # TODO necessary? why? also relevant on HV version, which i think has FIO0/1
@@ -162,53 +178,56 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     # Set the FIO0 and FIO1 to Analog (d3 = b00000011)
     # At least with my U3-HV, printing the output of this function is the same
     # whether FIOAnalog=3 or not, with 'FIOAnalog' == 15 in both cases.
+
     d.configIO(FIOAnalog=3)
 
     if verbose:
         print("Configuring U3 stream")
+
     # https://labjack.com/support/datasheets/u3/hardware-description/ain/channel_numbers
     # TODO determine when you'd want the behavior achieved by setting
     # NChannel=32 (see channel_numbers link above)
+
     single_ended_negative_channel = 31
     n_channels = [single_ended_negative_channel] * len(input_channels)
+
     # TODO TODO test that ScanFrequency should actually accept the frequency
     # divided by # of channels, and not the raw max scan frequency!!!
+
     d.streamConfig(NumChannels=len(input_channels), PChannels=input_channels,
         NChannels=n_channels, Resolution=resolution_index,
         ScanFrequency=scan_frequency
     )
 
-    # Both of these device attributes are set in the above `d.streamConfig`
-    # call.
+    # Both of these device attributes are set in the above `d.streamConfig` call.
+
     samples_per_request = d.streamSamplesPerPacket * d.packetsPerRequest
+
     # TODO TODO test whether rhs should be max_scan_freq or scan_frequency
+
     request_s = samples_per_request * (1 / max_scan_freq)
     if duration_s is not None:
         n_requests = int(math.ceil(duration_s / request_s))
     else:
         n_requests = None
 
-    # Time it takes to sample all the requested input channels.
+    # Time it takes to sample all the requested input channels:
     all_channel_sample_dt = 1 / scan_frequency
 
     if verbose:
-        print('samples_per_request:', samples_per_request)
-        print('samples_per_request / len(input_channels):',
-            samples_per_request / len(input_channels)
-        )
-        print('n_requests:', n_requests)
-        print('max_scan_freq:', max_scan_freq)
-        print('max_scan_freq / len(input_channels):',
-            max_scan_freq / len(input_channels)
-        )
-        print('duration_s:', duration_s)
-        # Since we need the last request to finish, even if it would bring our
-        # sample total above the total we are effectively requesting with
-        # duration_s.
-        actual_duration_s = request_s * n_requests
-        print('actual_duration_s:', actual_duration_s)
+        print("samples_per_request:", samples_per_request)
+        print("samples_per_request / len(input_channels):", samples_per_request / len(input_channels))
+        print("n_requests:", n_requests)
+        print("max_scan_freq:", max_scan_freq)
+        print("max_scan_freq / len(input_channels):", max_scan_freq / len(input_channels))
+        print("duration_s:", duration_s)
 
-        print('all_channel_sample_dt:', all_channel_sample_dt)
+        # Since we need the last request to finish, even if it would bring our
+        # sample total above the total we are effectively requesting with duration_s.
+        actual_duration_s = request_s * n_requests
+        print("actual_duration_s:", actual_duration_s)
+
+        print("all_channel_sample_dt:", all_channel_sample_dt)
 
     channel_names = [get_channel_name(d, i) for i in sorted(input_channels)]
 
@@ -220,29 +239,25 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     channel2column_names = dict(zip(channel_names, column_names))
 
     if time_col:
-        column_names = ['time_s'] + column_names
+
+        column_names = ["time_s"] + column_names
+
         # Starting from 0 within each request. Time offset will be added to this
         # before they are written to CSV rows along with measured data.
-        request_times = np.arange(
-            # If we start at 0 and use the same step, the stop will be
-            # request_s - all_channel_sample_dt.
-            start=all_channel_sample_dt,
-            stop=(request_s + all_channel_sample_dt),
-            step=all_channel_sample_dt
-        )
-        assert (len(request_times) ==
-            int(samples_per_request / len(input_channels)))
+        # If we start at 0 and use the same step, the stop will be
+        # request_s - all_channel_sample_dt.
+        request_times = np.arange(start=all_channel_sample_dt,
+                                  stop=(request_s + all_channel_sample_dt),
+                                  step=all_channel_sample_dt)
+        assert (len(request_times) == int(samples_per_request / len(input_channels)))
         last_time_s = 0.0
 
     if verbose:
-        print('column_names:', column_names)
-        print('channel2column_names:', channel2column_names)
+        print("column_names:", column_names)
+        print("channel2column_names:", channel2column_names)
 
     if not overwrite and os.path.exists(csv_path):
-        raise IOError(
-            'csv_path={} already exists (and overwrite=False)!'.format(
-            csv_path
-        ))
+        raise IOError(f"csv_path={csv_path} already exists and overwrite is set to False!")
 
     # Can't use the 3rd party `future` library `open` to add the `newline`
     # argument for python2, because then `writeheader()` (and probably other
@@ -254,7 +269,7 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     else:
         open_kwargs = dict()
 
-    csv_file_handle = open(csv_path, 'w', **open_kwargs)
+    csv_file_handle = open(csv_path, "w", **open_kwargs)
     atexit.register(csv_file_handle.close)
 
     csv_writer = csv.DictWriter(csv_file_handle, fieldnames=column_names)
@@ -287,20 +302,18 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
     request_count = 0
     packet_count = 0
     for r in d.streamData():
+        
         if r is not None:
+
             if r["errors"] != 0:
-                warnings.warn("Errors counted: {} ; {}".format(
-                    r["errors"], datetime.now()
-                ))
+                warnings.warn(f"Errors counted: {r['errors']} ; {datetime.now()}")
 
             if r["numPackets"] != d.packetsPerRequest:
-                warnings.warn("----- UNDERFLOW : {} ; {}".format(
-                      r["numPackets"], datetime.now()
-                ))
+                warnings.warn(f"----- UNDERFLOW : {r['numPackets']} ; {datetime.now()}")
 
             if r["missed"] != 0:
-                missed += r['missed']
-                warnings.warn("+++ Missed {}".format(r["missed"]))
+                missed += r["missed"]
+                warnings.warn(f"+++ Missed {r['missed']}")
 
             # TODO only data not used is 'firstPacket':
             # "The PacketCounter value in the first USB packet."
@@ -315,8 +328,7 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
             row_data_lists = [r[s] for s in channel_names]
 
             if time_col:
-                row_data_lists = \
-                    [list(request_times + last_time_s)] + row_data_lists
+                row_data_lists = [list(request_times + last_time_s)] + row_data_lists
                 last_time_s += request_s
 
             row_dicts = [dict(zip(column_names, row))
@@ -325,10 +337,9 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
             csv_writer.writerows(row_dicts)
 
             request_count += 1
-            packet_count += r['numPackets']
+            packet_count += r["numPackets"]
 
-            if (_finish_up or
-                (n_requests is not None and request_count >= n_requests)):
+            if (_finish_up or (n_requests is not None and request_count >= n_requests)):
 
                 break
         else:
@@ -336,51 +347,45 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
             # This only happens if your stream isn't faster than the USB
             # read timeout, ~1 sec.
             # TODO should i be warning / erring in this case?
-            print("No data ; {}".format(datetime.now()))
+            print(f"No data ; {datetime.now()}")
 
     if verbose:
+
         stop = datetime.now()
-
         sampleTotal = packet_count * d.streamSamplesPerPacket
-
         scanTotal = sampleTotal / len(input_channels)
-        print("%s requests with %s packets per request with %s samples "
-            "per packet = %s samples total." % (request_count,
-            (float(packet_count)/request_count),
-            d.streamSamplesPerPacket, sampleTotal
-        ))
-        print("%s samples were lost due to errors." % missed)
+
+        print(f"{request_count} requests with {float(packet_count) / request_count} packets per request \
+            with {d.streamSamplesPerPacket} samples per packet = {sampleTotal} samples total.")
+        print(f"{missed} samples were lost due to errors.")
+
         sampleTotal -= missed
-        print("Adjusted number of samples = %s" % sampleTotal)
 
-        print('sampleTotal * all_channel_sample_dt = ',
-            sampleTotal * all_channel_sample_dt
-        )
+        print(f"Adjusted number of samples = {sampleTotal}")
+        print(f"sampleTotal * all_channel_sample_dt = {sampleTotal * all_channel_sample_dt}")
 
-        runTime = ((stop-start).seconds +
-            float((stop-start).microseconds) / 1000000
-        )
+        runTime = ((stop-start).seconds + float((stop-start).microseconds) / 1000000)
+
         # TODO why does this difference seem to depend on
         # actual_duration_s?  does that mean that the timing between
         # samples is wrong in some places, or just that the startup /
         # shutdown of the stream takes longer in some cases? how to
         # test? maybe measure a reference square wave ~2-4x slower than
         # sample rate?
-        print('runTime - actual_duration_s:',
-            runTime - actual_duration_s
-        )
-        print("The experiment took %s seconds." % runTime)
-        print("Actual Scan Rate = %s Hz" % scan_frequency)
+
+        print(f"runTime - actual_duration_s: {runTime - actual_duration_s}")
+        print(f"The experiment took {runTime} seconds.")
+        print(f"Actual Scan Rate = {scan_frequency} Hz")
+
         # TODO TODO what causes discrepancies between these and
         # requested scan rate? should i warn if there's enough of a
         # difference? important? any offset between values yielded by
         # streamData, or just at beginning / end?
         # TODO test again now that break doesn't wait for one un-used yield
         # of the stream object
-        print("Timed Scan Rate = %s scans / %s seconds = %s Hz" %
-              (scanTotal, runTime, float(scanTotal)/runTime))
-        print("Timed Sample Rate = %s samples / %s seconds = %s Hz" %
-              (sampleTotal, runTime, float(sampleTotal)/runTime))
+        
+        print(f"Timed Scan Rate = {scanTotal} scans / {runTime} seconds = {float(scanTotal / runTime)} Hz"
+        print(f"Timed Sample Rate = {sampleTotal} samples / {runTime} seconds = {float(sampleTotal)/runTime)} Hz"
 
     # The atexit handlers will run after the labjack node that calls this
     # function exits. Letting SIGTERM (or maybe even un-handled SIGINT?) kill
@@ -389,11 +394,13 @@ def stream_to_csv(csv_path, duration_s=None, input_channels=None,
 
 
 if __name__ == '__main__':
-    #duration_s = None
+
     duration_s = 10.0
     input_channels = [0]
-    input_channel_names = ['pid']
-    stream_to_csv('test.csv', duration_s=duration_s,
-        input_channels=input_channels, input_channel_names=input_channel_names,
-        overwrite=True, verbose=True
-    )
+    input_channel_names = ["PID"]
+    stream_to_csv("test.csv", 
+                  duration_s=duration_s,
+                  input_channels=input_channels, 
+                  input_channel_names=input_channel_names,
+                  overwrite=True, 
+                  verbose=True)
