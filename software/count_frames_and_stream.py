0e0d4cba1a5888631c8a8fb58d0e91e57153ef88
Han Kim <hankim@caltech.edu>
2021-01-13 16:27:43 -0800

    add port and freq as args

--- software/count_frames_and_stream.py
+++ software/count_frames_and_stream.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Demos U3-HV's counter function. 
-Counts digital inputs into the FIO4 pin. 
-Useful for counting hardware trigger outputs to cams (i.e. frames)
+Demos simultaneous streaming and counting on the U3-HV. 
+Counts digital inputs into the FIO4 pin (Counter0).
+Useful for counting hardware trigger outputs to cams (i.e. frames).
 """
 
 import datetime
@@ -22,16 +22,22 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("duration", type=float,
         help="Duration (s) of the PID acquisition.")
+    parser.add_argument("port",
+        help="Path to the ATMega328P trigger's port, e.g. /dev/ttyUSB0")
+    parser.add_argument("frequency",
+        help="Frequency (Hz) of the trigger, e.g. frame rate.")
     args = parser.parse_args()
     
     duration = args.duration
+    port = args.port
+    frequency = args.frequency
 
     # Start the DAQ stream and cam trigger:
-    stream_to_csv("test.csv", 
+    stream_to_csv("stream.csv", 
                 duration_s=duration,
                 input_channels=[0, 210, 224], 
                 input_channel_names={0: "PID", 210: "DAQ count", 224: "TC_Capture"},
-                external_trigger={"port":"/dev/ttyUSB0", "freq":100, "width":10},
+                external_trigger={"port":port, "freq":frequency, "width":10},
                 do_overwrite=True, 
                 is_verbose=False)
 
