4dd27ccbef302d2720d991578c9403c5c747c18f
Han Kim <hankim@caltech.edu>
2021-02-20 17:36:36 -0800

    rm arg for 328p port path

--- software/count_frames_and_stream.py
+++ software/count_frames_and_stream.py
@@ -7,13 +7,14 @@ Streams analog inputs into the AIN0 pin, and counts digital
 inputs into the FIO4 pin (Counter0).
 
 Example command:
-./count_frames_and_stream.py stream.csv 5 /dev/ttyUSB0
+./count_frames_and_stream.py stream.csv 5 
 """
 
 import datetime
 import time
 import argparse
 import threading
+from os.path import expanduser
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -29,13 +30,10 @@ def main():
         help="Path to which to stream the .csv. Saves during acquisition.")
     parser.add_argument("duration",
         help="Duration (s) of the DAQ stream. If None, will stream until exited (ctrl+c).")
-    parser.add_argument("port",
-        help="Path to the ATMega328P trigger's port, e.g. /dev/ttyUSB0")
     args = parser.parse_args()
     
-    csv_path = args.csv_path
+    csv_path = expanduser(args.csv_path)
     duration = args.duration
-    port = args.port
 
     # TODO : Don't run more than one counter and/or timer. The required multiple 224 
     # channels means I have to make some fixes.
@@ -46,13 +44,15 @@ def main():
     else:
         duration = float(duration)
 
+    assert csv_path.endswith(".csv"), f"{csv_path} is not a .csv file"
+
     # Start the DAQ stream and cam trigger:
     stream_to_csv(csv_path=csv_path, 
                   duration_s=duration,
                   input_channels=[0, 210, 224], 
                   input_channel_names={0: "PID (V)", 210: "DAQ count", 224: "16-bit roll-overs"},
                   times="absolute",
-                  external_trigger={"port":port, "freq":100, "width":10},
+                  external_trigger={"port":"/dev/ttyUSB0", "freq":100, "width":10},
                   do_overwrite=True, 
                   is_verbose=True)
 
